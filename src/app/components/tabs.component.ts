5802065764314942a96a744f6bc4774f253d12c4
web-dave <dave@webdave.de>
2022-10-28 13:39:23 +0200

    grid list

--- src/app/components/tabs.component.ts
+++ src/app/components/tabs.component.ts
@@ -5,13 +5,41 @@ import { FeatureService } from '../feature.service';
 @Component({
   selector: 'app-tabs',
   template: `
-    <mat-tab-group>
+    <mat-tab-group [style.background]="'hotpink'">
       <mat-tab label="Tab 1" #one>
         <ng-container *ngIf="one.isActive">
-          <app-display></app-display>
-          <app-text text="Hallo"></app-text>
-          <app-filter (filter)="service.filter($event, 'tabs')"></app-filter>
-          <app-table [data]="(service.data$ | async) || []"></app-table>
+          <mat-grid-list cols="4" rowHeight="100px">
+            <mat-grid-tile
+              colspan="2"
+              rowspan="1"
+              [style.background]="'lightblue'"
+            >
+              <app-display></app-display>
+            </mat-grid-tile>
+            <mat-grid-tile
+              colspan="2"
+              rowspan="1"
+              [style.background]="'lightgreen'"
+              ><app-text text="Hallo"></app-text>
+            </mat-grid-tile>
+
+            <mat-grid-tile
+              colspan="4"
+              rowspan="2"
+              [style.background]="'#DDBDF1'"
+            >
+              <app-filter
+                (filter)="service.filter($event, 'tabs')"
+              ></app-filter>
+            </mat-grid-tile>
+            <mat-grid-tile
+              colspan="4"
+              rowspan="4"
+              [style.background]="'#DDBDF1'"
+            >
+              <app-table [data]="(service.data$ | async) || []"></app-table>
+            </mat-grid-tile>
+          </mat-grid-list>
         </ng-container>
       </mat-tab>
       <mat-tab label="Tab 2"> Content 2 </mat-tab>
